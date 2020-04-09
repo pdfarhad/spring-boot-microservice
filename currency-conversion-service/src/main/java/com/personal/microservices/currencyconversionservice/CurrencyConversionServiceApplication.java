b9b1d34758c25d62ce1d4d7e88c7c5a2cbaa9388
farhad alam <farhad.alam@shohoz.com>
2020-04-09 07:11:31 +0600

    Ribbon naming server added

--- currency-conversion-service/src/main/java/com/personal/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/personal/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.personal.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients("com.personal.microservices.currencyconversionservice")
+@EnableDiscoveryClient 
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
